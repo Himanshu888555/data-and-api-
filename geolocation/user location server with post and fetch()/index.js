6161801ef468ee3bf413bab6149b13a392205856
Himanshu888555 <Himanshu888555@gmail.com>
2020-06-05 19:51:24 +0530

    do it

---
+++ geolocation/user location server with post and fetch()/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app= express();
+
+app.listen(3000,()=>
+console.log('listening at 3000'));
+
+
+app.use(express.static('public'));
+
+app.post('/api',(request,response)=>{
+    console.log(request.body);
+})
